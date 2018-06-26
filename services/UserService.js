fa105635e36385b975aa782409b264e81701cc9b
Aria Groult <aria.groult@outlook.fr>
2018-06-26 19:00:45 +0200

    working on it

--- services/UserService.js
+++ services/UserService.js
@@ -22,28 +22,11 @@ const UserService = {
   },
 
   connect: (req, res, next, user) => {
-    let userData = {
-      email: user.email
-    }
-    let userToken = AuthenticationService.getUserToken(userData)
-    req.session.userToken = userToken;
-    res.sendStatus(200)
+
   },
 
   isUserConnected: (req, res, next) => {
-    //TEST VALUES
-    let user = {
-      email: "aria.groult@outlook.fr",
-      password: "azerty"
-    }
-    let token = jwt.sign(user, 'shhhh'); // Change secret
-    //TO DELETE
-    let userData = AuthenticationService.decodeUserToken(token)
-
-    if(typeof(userData.email) !== undefined && userData.email !== "")
-    {
-      // ADD correct http code for authentification done
-    }
+    
   }
 }
 
